1e080c7810715d55c568a49b4034ada6b7abe838
MINYUKYUNG <minew0223@gmail.com>
2023-01-23 04:31:30 +0900

    Setting: 초기 환경 셋팅

---
+++ .eslintrc.js
@@ -0,0 +1,76 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/extensions': [
+      2,
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      2,
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+        ],
+      },
+    ],
+    'react-hooks/exhaustive-deps': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'react/no-array-index-key': 'off',
+    'jsx-a11y/no-noninteractive-tabindex': 'off',
+    'no-param-reassign': 'off',
+    'consistent-return': 'off',
+    'no-alert': 'off',
+    'global-require': 'off',
+    'import/prefer-default-export': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    indent: 'off',
+    '@typescript-eslint/indent': ['error', 2],
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'no-useless-catch': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'no-shadow': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+        moduleDirectory: ['node_modules', '@types'],
+      },
+      typescript: {},
+    },
+  },
+};
