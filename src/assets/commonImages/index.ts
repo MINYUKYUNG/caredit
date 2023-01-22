86c651ea81a719c3b39d41c86000f86ac574451b
MINYUKYUNG <minew0223@gmail.com>
2023-01-23 04:36:38 +0900

    Feature: 공통 버튼 이미지 파일 추가

---
+++ src/assets/commonImages/index.ts
@@ -0,0 +1,4 @@
+export { default as appstoreImage } from './appstoreImage.jpg';
+export { default as googleplayImage } from './googleplayImage.jpg';
+export { default as headerImage } from './headerImage.jpg';
+export { default as naverImage } from './naverImage.jpg';
