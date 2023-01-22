ca3723d2b86bd6058378464fbaf2b8d690161ffb
MINYUKYUNG <minew0223@gmail.com>
2023-01-23 04:42:09 +0900

    Feature: 솔루션 페이지 추가

---
+++ src/pages/Solution/index.tsx
@@ -0,0 +1,20 @@
+import * as S from './style';
+import {
+  Header,
+  MenuBar,
+  Service,
+  Footer,
+} from '../../components';
+
+function Solution() {
+  return (
+    <S.SolutionContainer>
+      <Header />
+      <MenuBar />
+      <Service />
+      <Footer />
+    </S.SolutionContainer>
+  );
+}
+
+export default Solution;
